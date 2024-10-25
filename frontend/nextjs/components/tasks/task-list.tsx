a45a7cb2944d59fbeb2350ed14ab95947a996ca7
m-danya <danila-mikh@ya.ru>
2024-10-25 19:28:43 +0300

    Make tasks draggable and save isCompleted state

--- frontend/nextjs/components/tasks/task-list.tsx
+++ frontend/nextjs/components/tasks/task-list.tsx
@@ -1,32 +1,98 @@
-import { Task } from "@/components/tasks/task";
+"use client";
 
-const tasks = [
-  { name: "Watch 'BoJack Horseman'" },
-  { name: "Task with description", description: "some description here" },
-  { name: "Clean the house" },
-  { name: "Some completed archived task" },
-  { name: "Go to the gym", description: "💪🏻💪🏻💪🏻" },
-  { name: "A task with date", date: "2024-12-28" },
+import { Task } from "@/components/tasks/task";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { useState } from "react";
+
+const exampleTasks = [
+  { id: 1, name: "Watch 'BoJack Horseman'", isCompleted: true },
+  {
+    id: 2,
+    name: "Task with description",
+    description: "some description here",
+    isCompleted: true,
+  },
+  { id: 3, name: "Clean the house" },
+  { id: 4, name: "Some completed archived task" },
+  { id: 5, name: "Go to the gym", description: "💪🏻💪🏻💪🏻" },
+  { id: 6, name: "A task with date", date: "2024-12-28" },
 ];
 
 export function TaskList() {
+  const [tasks, setTasks] = useState(exampleTasks);
+  const dndSensors = useSensors(
+    useSensor(PointerSensor, { activationConstraint: { distance: 1 } }),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  function handleToggleTaskCompleted(new_task) {
+    setTasks((tasks) =>
+      tasks.map((task) => {
+        if (task.id === new_task.id) {
+          return new_task;
+        } else {
+          return task;
+        }
+      })
+    );
+  }
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setTasks((tasks) => {
+        const oldIndex = tasks.findIndex((task) => task.id === active.id);
+        const newIndex = tasks.findIndex((task) => task.id === over.id);
+
+        return arrayMove(tasks, oldIndex, newIndex);
+      });
+    }
+  }
+
   return (
-    <div className="items-center flex-col">
-      <div className="xl:px-40">
-        <div className="flex items-center justify-between">
-          <h1 className="text-xl font-semibold md:text-2xl">Tasks</h1>
+    <DndContext
+      sensors={dndSensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+        <div className="items-center flex-col">
+          <div className="xl:px-40">
+            <div className="flex items-center justify-between">
+              <h1 className="text-xl font-semibold md:text-2xl">Tasks</h1>
+            </div>
+            <div className="flex flex-col py-4">
+              {tasks.map((task) => (
+                <>
+                  <div>
+                    <Task
+                      task={task}
+                      handleToggleTaskCompleted={handleToggleTaskCompleted}
+                    />
+                  </div>
+                  <hr className="border-gray-200 dark:border-white" />
+                </>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col py-4">
-          {tasks.map((task) => (
-            <>
-              <div>
-                <Task task={task} />
-              </div>
-              <hr className="border-gray-200 dark:border-white" />
-            </>
-          ))}
-        </div>
-      </div>
-    </div>
+      </SortableContext>
+    </DndContext>
   );
 }
--- frontend/nextjs/components/tasks/task.tsx
+++ frontend/nextjs/components/tasks/task.tsx
@@ -1,15 +1,44 @@
-import { Checkbox } from "@/components/ui/checkbox";
+"use client";
 
-export function Task({ task }) {
+import { Checkbox } from "@/components/ui/checkbox";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+export function Task({ task, handleToggleTaskCompleted }) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: task.id });
+
+  const dndStyle = {
+    transform: CSS.Translate.toString(transform),
+    transition,
+  };
+
   return (
-    <div className="py-3.5 px-2 text-small">
-      <div className="flex items-center justify-start w-full">
-        <Checkbox className="mx-2 w-5 h-5 rounded-xl" />
-        <div className="flex flex-col items-center">{task.name}</div>
+    <div
+      ref={setNodeRef}
+      style={dndStyle}
+      {...attributes}
+      {...listeners}
+      key={task.id}
+    >
+      <div className="py-3.5 px-2 text-small">
+        <div className="flex items-center justify-start w-full">
+          <Checkbox
+            className="mx-2 w-5 h-5 rounded-xl"
+            checked={task.isCompleted}
+            onCheckedChange={(e) =>
+              handleToggleTaskCompleted({
+                ...task,
+                isCompleted: e,
+              })
+            }
+          />
+          <div className="flex flex-col items-center">{task.name}</div>
+        </div>
+        {task.description && (
+          <div className="ml-9 text-gray-400 pt-0.5">{task.description}</div>
+        )}
       </div>
-      {task.description && (
-        <div className="ml-9 text-gray-400 pt-0.5">{task.description}</div>
-      )}
     </div>
   );
 }
