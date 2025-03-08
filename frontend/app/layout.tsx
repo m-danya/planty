d47dd409ff0867c17fd9fa5a401a8affcce728af
m-danya <danila-mikh@ya.ru>
2025-03-08 13:20:44 +0300

    Use Next.js favicon functionality instead of pure React

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Head from "next/head";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,26 +13,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
       <body>{children}</body>
     </html>
   );
